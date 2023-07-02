1496f5ea5a5b0ea1b6a529df10db29d700809299
mrcheets <frank.chitwa@gmail.com>
2023-07-02 11:19:59 -0500

    getting uncle

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::ptr::{null_mut};
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 enum Color {
     RED,
     BLACK,
@@ -9,6 +9,7 @@ enum Color {
 #[derive(Debug)]
 struct Node {
     data: u32,
+    color: Color,
     parent: *mut Node,
     left: *mut Node,
     right: *mut Node,
@@ -23,6 +24,7 @@ impl Node {
     fn new(data: u32) -> Self {
         Self {
             data: data,
+            color: Color::RED,
             parent: null_mut(),
             left: null_mut(),
             right: null_mut(),
@@ -32,6 +34,7 @@ impl Node {
     fn insert(&mut self, node: &mut Node) {
         if self.data <= node.data {
             if self.right.is_null() {
+                    node.color = Color::RED;
                     node.parent = self;
                     self.right = node;
             } else {
@@ -42,6 +45,7 @@ impl Node {
             }
         } else {
             if  self.left.is_null() {
+                    node.color = Color::RED;
                     node.parent = self;
                     self.left = node;
             } else {
@@ -63,6 +67,7 @@ impl RedBlackTree {
 
     fn insert_node(&mut self, node: &mut Node) {
         if self.root.is_null() {
+            node.color = Color::BLACK;
             self.root = node;
         } else {
             unsafe {
@@ -74,7 +79,37 @@ impl RedBlackTree {
     }
 
     fn balance_tree(&mut self, start_node: &mut Node) {
+        if start_node.parent.is_null(){
+            return;
+        } else {
+            unsafe {
+                let parent = &(*start_node.parent);
+                if parent.color == Color::BLACK {
+                    return;
+                } else {
+                    let uncle = self.get_uncle(start_node);
+                    println!("Parent color is red");
+                    println!("Uncle: {:?}", uncle);
 
+                }
+            }
+        }
+    }
+
+    fn get_uncle(&mut self, node: &mut Node) -> *mut Node {
+        unsafe {
+            let parent = &(*node.parent);
+            let grand_parent = parent.parent;
+            if grand_parent.is_null() {
+                null_mut()
+            } else {
+                if parent.data >= (*grand_parent).data {
+                    (*grand_parent).left
+                } else {
+                    (*grand_parent).right
+                }
+            }
+        }
     }
 }
 
@@ -84,11 +119,18 @@ fn main() {
     let mut node1 = Node::new(3);
     let mut node2 = Node::new(5);
     let mut node3 = Node::new(2);
+    let mut node4 = Node::new(4);
     rb_tree.insert_node(&mut node1);
+    rb_tree.balance_tree(&mut node1);
     rb_tree.insert_node(&mut node2);
+    rb_tree.balance_tree(&mut node2);
     rb_tree.insert_node(&mut node3);
-    println!("{:?}", rb_tree);
+    rb_tree.balance_tree(&mut node3);
+    rb_tree.insert_node(&mut node4);
+    rb_tree.balance_tree(&mut node4);
+    
+    /*println!("{:?}", rb_tree);
     println!("{:?}", unsafe{ &(*rb_tree.root) });
     println!("{:?}", unsafe{ &(*(*rb_tree.root).right) });
-    println!("{:?}", unsafe{ &(*(*rb_tree.root).left) });
+    println!("{:?}", unsafe{ &(*(*rb_tree.root).left) });*/
 }
